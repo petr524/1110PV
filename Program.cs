241a270178ddf86992b6cf369f0404821cca9713
petr524 <128919859+petr524@users.noreply.github.com>
2023-10-11 11:00:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _1110
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = GenerateRandomNumbers(10, 1, 100);
+
+            double average = CalculateAverage(numbers);
+            Console.WriteLine($"Aritmetický průměr: {average}");
+
+            int min = FindMinimum(numbers);
+            Console.WriteLine($"Nejmenší číslo: {min}");
+
+            int divisor = 5;
+            int divisibleCount = CountDivisibleBy(numbers, divisor);
+            Console.WriteLine($"Počet čísel dělitelných {divisor}: {divisibleCount}");
+        }
+
+        static int[] GenerateRandomNumbers(int count, int min, int max)
+        {
+            Random random = new Random();
+            int[] numbers = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = random.Next(min, max + 1);
+            }
+            return numbers;
+        }
+
+        public static double CalculateAverage(int[] numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return (double)sum / numbers.Length;
+        }
+
+        public static int FindMinimum(int[] numbers)
+        {
+            int min = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            return min;
+        }
+
+        public static int CountDivisibleBy(int[] numbers, int divisor)
+        {
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (number % divisor == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1110
+{
+    internal class User: IComparable<User>
+    {
+        private String nickname;
+        private String password;
+
+        public string Nickname { get => nickname; set => nickname = value; }
+        public string Password { get => password; set => password = value; }
+
+        public User(string nickname, string password) 
+        { 
+            this.nickname = nickname;
+            this.password = password;
+        }
+        public static User CreateUser(String nickname, String password)
+        {
+            if (nickname.Length < 3)
+            {
+                throw new ArgumentException("Nickname musi mit alespon 3 znaky");
+            }
+            if (password.Length < 5)
+            {
+                throw new ArgumentException("Password musi mit alespon 5 znaku;");
+            }
+            return new User(nickname, password);
+        }
+        public int CompareTo(User u) 
+        {
+            if(u == null) return 1;
+            return string.Compare(this.nickname, u.nickname);
+        }
+        public override String ToString()
+        {
+            return nickname+"-";
+        }
+    }
+
+}
